8b38a3931dc534e7075f4a3d820707c460dff469
sohel syed <syed.sohel@learnbay.co>
2025-10-21 15:49:39 +0530

    add poppins font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Theme } from "@radix-ui/themes";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import Footer from "./components/Footer";
 import Navigation from "./components/navigation";
 import "./globals.css";
@@ -13,6 +13,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
 export const metadata = {
   title: "InnoKidz - Future Entrepreneurs Program | AI & Coding for Kids 8-17",
   description:
@@ -23,7 +29,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${poppins.variable} ${geistSans.variable} ${geistMono.variable}  antialiased`}
       >
         <Navigation />
         <Theme>{children}</Theme>
